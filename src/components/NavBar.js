0d3a3799cbbeee3255841847ab1a47bc954adf56
utkarshanik <utkarshanikam9@gmail.com>
2023-04-29 12:36:42 +0530

    this

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,10 +51,10 @@ const NavBar=(props)=>{
 
       </ul>
 
-      <form className="d-flex" role="search">
+      {/* <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-success" type="submit">Search</button>
-      </form>
+      </form> */}
              
     </div>
 
